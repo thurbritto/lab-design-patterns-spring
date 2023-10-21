a342ece7cb65e7c32a07c39d14915bba8cc70581
Arthur Britto <103975045+thurbritto@users.noreply.github.com>
2023-10-21 02:59:00 -0300

    adicionando base do projeto

---
+++ src/test/java/dio/challenge/LabDesignPatternsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDesignPatternsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
